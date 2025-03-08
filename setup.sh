e6da72cd736fda19ba021609b651aa1f66bd1aba
Tim Green <itimky@gmail.com>
2025-03-08 00:15:12 -0300

    format setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@
 DIR=$(cd $(dirname $0) > /dev/null && pwd)
 
 # zsh
-ln -sf $DIR/zshrc ~/.zshrc
-ln -sfn $DIR/zshrc.d ~/.zshrc.d
+ln -sf "${DIR}/zshrc" "${HOME}/.zshrc"
+ln -sfn "${DIR}/zshrc.d" "${HOME}/.zshrc.d"
 
 # vim
-ln -sf $DIR/vim/vimrc ~/.vimrc
-ln -sfn $DIR/vim ~/.vim
+ln -sf "${DIR}/vim/vimrc" "${HOME}/.vimrc"
+ln -sfn "${DIR}/vim" "${HOME}/.vim"
 
 # git
-ln -sf $DIR/gitconfig ~/.gitconfig
-ln -sf $DIR/gitignore ~/.gitignore
+ln -sf "${DIR}/gitconfig" "${HOME}/.gitconfig"
+ln -sf "${DIR}/gitignore" "${HOME}/.gitignore"
 
 # tmux
-ln -sf $DIR/tmux.conf ~/.tmux.conf
+ln -sf "${DIR}/tmux.conf" "${HOME}/.tmux.conf"
