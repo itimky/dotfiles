98ef479b89ca28559f82c25e4de8b044b3f7e067
Tim Green <itimky@gmail.com>
2025-03-08 01:53:25 -0300

    kube: configs from ~/.kube/*.yaml

--- zshrc.d/kube.zsh
+++ zshrc.d/kube.zsh
@@ -1,6 +1,6 @@
 KUBECONFIG="${HOME}/.kube/config"
 
-for CONFIG_FILE in ~/.kube/config.d/*.yaml; do
+for CONFIG_FILE in ~/.kube/*.yaml; do
   [ -r "${CONFIG_FILE}" ] && KUBECONFIG="${KUBECONFIG}:${CONFIG_FILE}"
 done
 
